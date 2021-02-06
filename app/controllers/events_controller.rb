d5f0e9600bd9bfea3d9af6580f16a7abefbb3c37
kostas-s <ksmonos@hotmail.com>
2021-02-06 21:44:36 +0200

    refactored event filtering using scopes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   before_action :get_attended_events, only: [:index, :show]
 
   def index
-    @events = Event.all
+    @events = Event.all.order('date')
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :host, foreign_key: 'user_id', class_name: 'User'
   has_many :invitations
   has_many :attendees, through: :invitations, source: :user
+
+  scope :past, -> { where('date < ?', DateTime.now) }
+  scope :future, -> { where('date >= ?', DateTime.now) }
+
 end
